14b919e82d6a915609588bf70abfec9531321519
navinkumarp <navinkumarp.91@gmail.com>
2022-02-11 14:43:53 +0530

    First commit.

---
+++ src/test/java/TestRectangle.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestRectangle {
+
+    @Test
+    public void testRectangleArea() {
+        Assert.assertEquals(1,1);
+    }
+}
