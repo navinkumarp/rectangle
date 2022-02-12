c26f39cd34a6e38bdd77345c62d77ac697b5f718
navinkumarp <navinkumarp.91@gmail.com>
2022-02-12 20:44:02 +0530

    Some more tests

--- src/test/java/TestRectangle.java
+++ src/test/java/TestRectangle.java
@@ -5,8 +5,22 @@ import org.testng.annotations.Test;
 public class TestRectangle {
 
     @Test
-    public void testRectangleArea_TwoAndThree_Six() {
+    public void testRectangleArea_threePointFiveAndThree_tenPointFive() {
+        Rectangle rectangle = new Rectangle((float) 3.5,(float) 3);
+        Assert.assertEquals(rectangle.getArea() ,10.5);
+    }
+
+    @Test
+    public void testRectangleArea_twoAndThree_six() {
         Rectangle rectangle = new Rectangle(2,3);
         Assert.assertEquals(rectangle.getArea() ,6);
     }
+
+    @Test
+    public void testRectangleArea_threePointTwoTwoAndThreePointTwo_tenPointFive() {
+        Rectangle rectangle = new Rectangle((float) 3.22,(float) 3.2);
+        Assert.assertEquals(rectangle.getArea() ,10.30, 0.01);
+    }
+
+
 }
