6559bc8a819aa6ec34d311065fa7e14eaa975b48
navinkumarp <navinkumarp.91@gmail.com>
2022-02-12 20:54:29 +0530

    More tests for Perimeter

--- src/test/java/TestRectangle.java
+++ src/test/java/TestRectangle.java
@@ -5,19 +5,19 @@ import org.testng.annotations.Test;
 public class TestRectangle {
 
     @Test
-    public void testRectangleArea_threePointFiveAndThree_tenPointFive() {
-        Rectangle rectangle = new Rectangle((float) 3.5,(float) 3);
-        Assert.assertEquals(rectangle.getArea() ,10.5);
-    }
-
-    @Test
     public void testRectangleArea_twoAndThree_six() {
         Rectangle rectangle = new Rectangle(2,3);
         Assert.assertEquals(rectangle.getArea() ,6);
     }
 
     @Test
-    public void testRectangleArea_threePointTwoTwoAndThreePointTwo_tenPointFive() {
+    public void testRectangleArea_threePointFiveAndThree_tenPointFive() {
+        Rectangle rectangle = new Rectangle((float) 3.5,(float) 3);
+        Assert.assertEquals(rectangle.getArea() ,10.5);
+    }
+
+    @Test
+    public void testRectangleArea_threePointTwoTwoAndThreePointTwo_tenPointThree() {
         Rectangle rectangle = new Rectangle((float) 3.22,(float) 3.2);
         Assert.assertEquals(rectangle.getArea() ,10.30, 0.01);
     }
@@ -26,6 +26,18 @@ public class TestRectangle {
     public void testRectanglePerimeter_threeAndThree_twelve() {
         Rectangle rectangle = new Rectangle( 3, 3);
         Assert.assertEquals(rectangle.getPerimeter() ,12);
+    }
+
+    @Test
+    public void testRectanglePerimeter_threePointFiveAndThree_tenPointFive() {
+        Rectangle rectangle = new Rectangle((float) 3.5,(float) 3);
+        Assert.assertEquals(rectangle.getPerimeter() ,13);
+    }
+
+    @Test
+    public void testRectanglePerimeter_threePointTwoTwoAndThreePointTwo_tenPointFive() {
+        Rectangle rectangle = new Rectangle( (float) 3.22, (float)3.2);
+        Assert.assertEquals(rectangle.getPerimeter() ,12.84, 0.01);
     }
 
 
